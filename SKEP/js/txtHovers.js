d17db5dec07681bfb1deea5a934e83f82ab29b28
Nicolás Mahecha <nicolasmahecha@misena.edu.co>
2015-05-13 12:14:01 -0500

    Nikodermus + Skep + Color Shine.apk

---
+++ SKEP/js/txtHovers.js
@@ -0,0 +1,9 @@
+(document).ready(function)
+
+{("#accion").mouseover(function(event)
+{("#elemento").addClass("animacion");});
+ 
+("#accion").mouseout(function(event)
+{("#elemento").removeClass("animacion");
+});
+});
