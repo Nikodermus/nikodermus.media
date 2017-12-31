1f08af0c2fd418c9979f84326a47beeac38d127f
Nicolás M. Pardo <nicolasm@nikodermus.media>
2017-12-31 14:48:15 -0500

    refactor(Global): 🎊   Opened branch for v2
    Deleted everything and created angular 5 + stylus app, will be deployed in Heroku

--- SKEP/js/txtHovers.js
+++
@@ -1,9 +0,0 @@
-(document).ready(function)
-
-{("#accion").mouseover(function(event)
-{("#elemento").addClass("animacion");});
- 
-("#accion").mouseout(function(event)
-{("#elemento").removeClass("animacion");
-});
-});
