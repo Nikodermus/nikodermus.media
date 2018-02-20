3ab269d8f830151901a8a9078c903521c61846a5
Nicolás M. Pardo <nicolasm@nikodermus.media>
2018-02-19 23:41:41 -0500

    feat(🔺  Global): Added wowjs and changed components order for a better display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,45 @@ import { Component, ViewEncapsulation } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  wheel_event = this.whichWheelEvent();
+
+  ngOnInit() {
+    window.addEventListener(this.wheel_event,
+      this.debounce(
+        (e) => {
+          console.log(e);
+        }
+      ));
+  }
+
+
+  private whichWheelEvent() {
+
+    let mouse_events = {
+      "onwheel": "wheel",
+      "onmousewheel": "mousewheel",
+    }
+
+    for (let t in mouse_events) {
+      if (window[t] !== undefined) {
+        return mouse_events[t];
+      }
+    }
+  }
+
+  private debounce(func, wait = 20, immediate = true) {
+    let timeout;
+    return function () {
+      let context = this,
+        args = arguments;
+      let later = function () {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+      };
+      let callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if (callNow) func.apply(context, args);
+    };
+  }
 }
