8866e1d5d5475a32d89f3e8824f281dc306c991d
Nicolás M. Pardo <nicolasm@nikodermus.media>
2018-03-20 19:37:50 -0500

    build(Heroku): Added heroku and express server

---
+++ server.js
@@ -0,0 +1,18 @@
+// server.js
+
+const express = require('express');
+const app = express();
+
+// Run the app by serving the static files
+// in the dist directory
+
+app.use(express.static(__dirname + '/dist'));
+
+// Start the app by listening on the default
+// Heroku port
+
+app.get('/*', function (req, res) {
+
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+app.listen(process.env.PORT || 8080);
