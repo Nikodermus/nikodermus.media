ad70bdf015411776298d84eb59d0b2bbfb92914b
Nicolas M. Pardo <nicolasm@nikodermus.media>
2020-08-14 16:36:10 -0500

    feat(pages/uses) Create redirect to github

---
+++ src/pages/uses.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+const Uses = () => {
+    useEffect(() => {
+        window.location.replace('https://github.com/Nikodermus/dotfiles');
+    }, []);
+
+    return null;
+};
+
+export default Uses;
